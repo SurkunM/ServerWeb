8bd6ade7f5d9573a4defe94aa6ccb47ab0acaa25
Sss <surkun1992@gmail.com>
2025-03-08 11:56:12 +0700

    Excel Persons

---
+++ Excel/ExcelMain.cs
@@ -0,0 +1,53 @@
+﻿using ClosedXML.Excel;
+
+namespace Excel;
+
+internal class ExcelMain
+{
+    private static void ConvertToExcelTable(List<Person> personsList)
+    {
+        if (personsList is null)
+        {
+            throw new ArgumentNullException(nameof(personsList));
+        }
+
+        using var workbook = new XLWorkbook();
+
+        var worksheet = workbook.AddWorksheet();
+        worksheet.Range("A1:D1").Merge();
+
+        worksheet.Cell("A1").SetValue("Сотрудники");
+        worksheet.Cell("A1").Style.Font.FontSize = 12;
+        worksheet.Cell("A1").Style.Alignment.Horizontal = XLAlignmentHorizontalValues.Center;
+        worksheet.Columns().AdjustToContents();
+
+        var table = worksheet.Cell("A2").InsertTable(personsList, "Сотрудники");
+
+        table.ShowAutoFilter = false;
+        table.Theme = XLTableTheme.TableStyleMedium5;
+
+        workbook.SaveAs("..\\..\\..\\ExcelFiles\\result.xlsx");
+    }
+
+    static void Main(string[] args)
+    {
+        var personsList = new List<Person>
+        {
+            new ("Иван", "Иванов", 44, 23001),
+            new ("Николай", "Абрамов", 35, 23011),
+            new ("Василий", "Степанов", 22, 23002),
+            new ("Петр", "Степанов", 25, 23004),
+            new ("Анна", "Абрамова", 31, 23012),
+            new ("Ольга", "Степанова", 42, 23038)
+        };
+
+        try
+        {
+            ConvertToExcelTable(personsList);
+        }
+        catch (IOException)
+        {
+            Console.WriteLine("Произошла ошибка при попытке сохранения excel-файла. Недостаточно прав для сохранения или файл уже открыт другим процессом");
+        }
+    }
+}
---
+++ Excel/Person.cs
@@ -0,0 +1,40 @@
+﻿namespace Excel;
+
+public class Person
+{
+    public string LastName { get; }
+
+    public string FirstName { get; }
+
+    public int Age { get; }
+
+    public int Phone { get; }
+
+    public Person(string firstName, string lastName, int age, int phone)
+    {
+        if (firstName is null)
+        {
+            throw new ArgumentNullException(nameof(firstName));
+        }
+
+        if (lastName is null)
+        {
+            throw new ArgumentNullException(nameof(lastName));
+        }
+
+        if (age <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(age), "Возраст не может быть отрицательным числом");
+        }
+
+        if (phone <= 0 || phone >= 100000)
+        {
+            throw new ArgumentOutOfRangeException(nameof(age), "Номер телефона не должен быть меньше или равно 0, или больше 100 000");
+        }
+
+        Age = age;
+        LastName = lastName;
+        FirstName = firstName;
+        Phone = phone;
+    }
+}
