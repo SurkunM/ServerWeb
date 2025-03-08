7dd28eadc630f21f6be858651f1c204b1db04523
Sss <surkun1992@gmail.com>
2025-03-08 20:20:37 +0700

    Unit test для методов GetLength(), Scalar() и конструктора(размерность)

--- UnitTestVector/VectorTests.cs
+++ UnitTestVector/VectorTests.cs
@@ -6,12 +6,39 @@ namespace UnitTestVector
     public sealed class VectorTests
     {
         [TestMethod]
-        public void TestLength()
+        public void LengthTest()
         {
-            double[] array2 = { 3, 3, 1 };
-            var vector = new Vector(array2);
+            const double epsilon = 1.0e-10;
+            double[] array = { 3, 3, 1 };
+            double vectorLength = Math.Sqrt(array.Sum(e => Math.Pow(e, 2)));
 
-            Assert.IsTrue(vector.GetLength() == 4.358898943540674, "Length must be 3");
+            var vector = new Vector(array);
+            Assert.IsTrue(vector.GetLength() - vectorLength <= epsilon, $"Длинна вектора не совпадает с {vectorLength}");
+        }
+
+        [TestMethod]
+        public void ConstructorTest()
+        {
+            Action action = () => new Vector(0);
+            Assert.ThrowsExactly<ArgumentException>(action);
+        }
+
+        [TestMethod]
+        public void MultiplyByScalarTest()
+        {
+            double[] array = { 3, 3, 1 };
+
+            var vector = new Vector(array);
+            var scalar = 2;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] *= scalar;
+            }
+
+            vector.MultiplyByScalar(scalar);
+
+            Assert.AreEqual(vector, new Vector(array));
         }
     }
 }
