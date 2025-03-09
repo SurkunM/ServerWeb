6d2f604b6f7b9664076b7c85baa554f119706da8
Sss <surkun1992@gmail.com>
2025-03-09 10:53:00 +0700

    .NetFramework Log

---
+++ NetFrameworkLog/NetFrameworkLogMain.cs
@@ -0,0 +1,26 @@
+﻿using NLog;
+using System;
+
+namespace NetFrameworkLog
+{
+    class NetFrameworkLogMain
+    {
+        static void Main(string[] args)
+        {
+            var logger = LogManager.GetCurrentClassLogger();
+            logger.Info("Программа запущена");
+
+            try
+            {
+                throw new Exception("Ошибка");
+            }
+            catch (Exception e)
+            {
+                logger.Error(e, "Произошла ошибка");
+            }
+
+            logger.Info("Программа закончена");
+            Console.ReadKey();
+        }
+    }
+}
