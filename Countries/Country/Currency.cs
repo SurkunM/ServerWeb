e20ec94f8af2ee61a0d17d7efc1a14078d3342cb
Sss <surkun1992@gmail.com>
2025-03-28 21:26:21 +0700

    удаление папки

---
+++ Countries/Country/Currency.cs
@@ -0,0 +1,19 @@
+﻿namespace CountriesJson.Country;
+
+internal class Currency
+{
+    public string Code { get; set; } = default!;
+
+    public string Name { get; set; } = default!;
+
+    public string Symbol { get; set; } = default!;
+
+    public override string ToString()
+    {
+        return $"""            
+        "code": {Code},
+        "name": {Name},
+        "symbol": {Symbol}            
+        """;
+    }
+}
