cce76280b24a108be2f3e16937f6ae79ed12d543
Sss <surkun1992@gmail.com>
2025-04-08 16:13:35 +0700

    Логгирование .Net

---
+++ LogTask/LogProgram.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog;
+using NLog.Extensions.Logging;
+
+namespace LogTask;
+
+internal class LogProgram
+{
+    public static void Main(string[] args)
+    {
+        var logger = LogManager.GetCurrentClassLogger();
+
+        try
+        {
+            using var servicesProvider = new ServiceCollection()
+                .AddTransient<Runner>()
+                .AddLogging(loggingBuilder =>
+                {
+                    loggingBuilder.ClearProviders();
+                    loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Information);
+                    loggingBuilder.AddNLog();
+                }).BuildServiceProvider();
+
+            var runner = servicesProvider.GetRequiredService<Runner>();
+
+            runner.StartAction();
+            runner.EndAction();
+
+            throw new Exception("Тестовое исключение");
+        }
+        catch (Exception e)
+        {
+            logger.Error($"Ошибка! {e}");
+        }
+    }
+}
---
+++ LogTask/Runner.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace LogTask;
+
+public class Runner
+{
+    private readonly ILogger<Runner> _logger;
+
+    public Runner(ILogger<Runner> logger)
+    {
+        _logger = logger;
+    }
+
+    public void StartAction()
+    {
+        _logger.LogWarning("Программа запущена");
+    }
+
+    public void EndAction()
+    {
+        _logger.LogInformation("Программа завершена");
+    }
+}
